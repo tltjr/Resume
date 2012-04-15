a98c05371882b419894c34f28657a3ca20bb3b26
unknown <Mal & Barb@Rommel-PC.(none)>
2012-04-15 15:14:40 -0700

    Initial commit

---
+++ Resume/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Resume.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Pdf()
+        {
+            var cd = new System.Net.Mime.ContentDisposition
+            {
+                FileName = @"thornton-resume.pdf", 
+                Inline = false, 
+            };
+            Response.AppendHeader("Content-Disposition", cd.ToString());
+            return File(@"..\Content\thornton-resume.pdf", "application/pdf");
+        }
+    }
+}
